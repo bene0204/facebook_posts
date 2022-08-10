d4b2a4bb904faa29e2ff7eeb13ac8b37dc64b044
DESKTOP-QKSE2O9\Márk <benem960802@gmail.com>
2022-08-10 09:48:25 +0200

    initial project

---
+++ src/main/java/com/benem/facebook_posts/FacebookPostsApplication.java
@@ -0,0 +1,13 @@
+package com.benem.facebook_posts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FacebookPostsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FacebookPostsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/benem/facebook_posts/FacebookPostsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.benem.facebook_posts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FacebookPostsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
